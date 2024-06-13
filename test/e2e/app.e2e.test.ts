c1321f621d16ab5460007e39a3ed5d55828e2df6
pashaman <plavel@gmail.com>
2024-06-13 19:34:40 +0700

    feat: covered api with e2e tests

---
+++ test/e2e/app.e2e.test.ts
@@ -0,0 +1,122 @@
+import request, { Response } from 'supertest';
+import { SignUpDto } from '../../src/dto';
+import { aladoServerOptions } from '../../src/config';
+import { app } from '../../src/application';
+
+const userInfo: SignUpDto = {
+  username: 'john_doe',
+  password: 'cant_remember',
+  bio: 'noname'
+};
+
+const updatedUserInfo: SignUpDto = {
+  username: 'john_snow',
+  password: 'new_password',
+  bio: 'some bio'
+};
+
+const apiUrl = `http://localhost:${aladoServerOptions.port}`
+
+describe('Example API  e2e test suite', () => {
+
+  let bearerToken: string;
+  let id: string;
+
+  beforeAll(() => {
+   app.start(() => {})
+  }, 30000);
+
+  afterAll(() => {
+    app.stop(() => {});
+  });
+
+  it(`Should allow to sign up`,  () => {
+    return request(apiUrl)
+      .post('/user')
+      .set('Accept', 'application/json')
+      .send(userInfo)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response: Response) => {
+        expect(response.body.id).toBeDefined();
+        expect(response.body.username).toEqual(userInfo.username);
+        expect(response.body.bio).toEqual(userInfo.bio);
+        id = response.body.id
+      })
+  });
+
+  it(`Shouldn't allow to make unauthorized requests`,  () => {
+    return request(apiUrl)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .then((response: Response) => {
+        expect(response.body.message).toBeDefined();
+      })
+  });
+
+  it(`Should allow to sign in`,  () => {
+    return request(apiUrl)
+      .post('/session')
+      .set('Accept', 'application/json')
+      .send({
+        password: userInfo.password,
+        username: userInfo.username
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response: Response) => {
+        expect(response.body.token).toBeDefined();
+        bearerToken = response.body.token
+      })
+  });
+
+  it(`Should allow authorized user to get user`,  () => {
+    return request(apiUrl)
+      .get(`/user/${id}`)
+      .set('Accept', 'application/json')
+      .set('Authorization', bearerToken)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response: Response) => {
+        expect(response.body.id).toBeDefined();
+        expect(response.body.username).toBeDefined();
+        expect(response.body.bio).toBeDefined();
+      })
+  });
+
+  it(`Should allow authorized user to get users list`,  () => {
+    return request(apiUrl)
+      .get(`/user`)
+      .set('Accept', 'application/json')
+      .set('Authorization', bearerToken)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response: Response) => {
+        expect(response.body.length).toBeDefined();
+        expect(response.body.length).toBeGreaterThan(0);
+      })
+  });
+
+  it(`Should allow authorized user update info`,  () => {
+    return request(apiUrl)
+      .put(`/user/${id}`)
+      .set('Accept', 'application/json')
+      .set('Authorization', bearerToken)
+      .send({
+        password: updatedUserInfo.password,
+        username: updatedUserInfo.username,
+        bio: updatedUserInfo.bio
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response: Response) => {
+        expect(response.body.id).toBeDefined();
+        expect(response.body.username).toEqual(updatedUserInfo.username);
+        expect(response.body.bio).toEqual(updatedUserInfo.bio);
+      })
+  });
+
+});
+
